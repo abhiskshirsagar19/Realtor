9081b2c77f36adee18fa271f30c9192683b8cbf2
Abhi <abhikshirsagar1999@gmail.com>
2024-07-06 07:04:51 +0530

    Sign in functionality added.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import { RiEyeOffLine, RiEyeLine } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({ email: "", password: "" });
   const { email, password } = formData;
+  const navigate = useNavigate();
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredentials.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad user credentials.");
+    }
   }
   return (
     <section>
@@ -24,7 +43,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[47%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
